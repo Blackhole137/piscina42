3bbbf73ef689277fb5223fe467cd0b6b4b15c765
Blackhole137 <lev.xrman@gmail.com>
2026-06-11 12:37:44 +0200

    c03

---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,23 @@
+unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int dest_len;
+	unsigned int src_len;
+
+	i = 0;
+	while (dest[i] != '\0' && i < size)
+		i++;
+	dest_len = i;
+	j = 0;
+	while (src[j] != '\0' && dest_len + j + 1 < size)
+	{
+		dest[dest_len + j] = src[j];
+		j++;
+	}
+	dest[dest_len + j] = '\0';
+	src_len = 0;
+	while (src[src_len] != '\0')
+		src_len++;
+	return (dest_len + src_len);
+}
